4e23d569cd818e0e9352d6f73484c3eae2baddd7
yowcow <yowcow@cpan.org>
2017-06-28 14:21:43 +0900

    tidy test

--- handler/websocket_test.go
+++ handler/websocket_test.go
@@ -11,19 +11,13 @@ import (
 )
 
 func TestWebSocket(t *testing.T) {
-	//w := httptest.NewRecorder()
-	req := httptest.NewRequest("GET", "/ws", nil)
-	req.Header.Add("upgrade", "websocket")
-	req.Header.Add("connection", "Upgrade")
-	req.Header.Add("sec-websocket-version", "13")
-	req.Header.Add("sec-websocket-key", "hogefugafoobar")
-
 	handler := http.HandlerFunc(WebSocket)
 	server := httptest.NewServer(handler)
 	defer server.Close()
 
 	dialer := &websocket.Dialer{}
-	conn, _, err := dialer.Dial(strings.Replace(server.URL, "http", "ws", 1), nil)
+	url := strings.Replace(server.URL, "http", "ws", 1)
+	conn, _, err := dialer.Dial(url, nil)
 	//defer conn.Close()
 
 	assert.Equal(t, nil, err)
